1786067160d1d51f8ccba2c2c9626df371080b31
Tom W <199689467+TomTomJW@users.noreply.github.com>
2025-12-28 16:44:00 -0500

    Added Simple UI

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let humanScore = 0;
 let computerScore = 0;
-playGame();
+
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorButton = document.querySelector("#scissors");
+let resultSection = document.querySelector(".roundOutcome");
+
+rockButton.addEventListener("click", () => {
+    playGame("rock");
+});
+
+paperButton.addEventListener("click", () => {
+    playGame("paper");
+});
+
+scissorButton.addEventListener("click", () => {
+    playGame("scissors");
+});
 
 function getComputerChoice() {
     let random = Math.random() * 100;
@@ -9,53 +25,38 @@ function getComputerChoice() {
     } else if ((random > 33) && (random < 66)) {
         return "paper";
     } else {
-        return "scissor";
+        return "scissors";
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter Rock Paper Or Scissors");
-    choice = choice.toLowerCase();
-    return choice;
-}
+function playRound(humanChoice, computerChoice) {
+    resultSection.replaceChildren();
 
-function playRound(humanChoice, computerChoice, round) {
-    // Line below clears the console. The console is cleared each rounded for cleanliness
-    console.clear();
-    console.log("Round: " + round);
+    let playerChoices = document.createElement("p");
+    playerChoices.textContent = "You chose " + humanChoice + " and the computer chose " + computerChoice;
+    playerChoices.style.cssText = "text-align: center;"
+    resultSection.appendChild(playerChoices);
+
+    let message = document.createElement("p");
+    message.style.cssText = "text-align: center;"
     if (
         ((computerChoice === "paper") && (humanChoice === "rock")) || 
         ((computerChoice === "rock") && (humanChoice === "scissors")) ||
         ((computerChoice === "scissors") && (humanChoice === "paper"))) {
         computerScore = computerScore + 1;
-        console.log("You have lost! Good luck next time")
+        console.log("You have lost! Good luck next time");
+        message.textContent = "You have lost! Good luck next time";
     } else if (computerChoice === humanChoice) {
-        console.log("There is a tie. Try again")
+        console.log("There is a tie. Try again");
+        message.textContent = "There is a tie. Try again";
     } else {
         humanScore = humanScore + 1;
-        console.log("You have won, congratulations!")
+        console.log("You have won, congratulations!");
+        message.textContent = "You have won, congratulations!";
     }
-    console.log("         ");
-    console.log("Human Choice: " + humanChoice);
-    console.log("Computer Choice: " + computerChoice);
-    console.log("      ");
-    console.log("Human score: " + humanScore);
-    console.log("Computer score: " + computerScore);
+    resultSection.appendChild(message);
 }
 
-function playGame() {
-    let round = 1;
-    while ((humanScore < 5) && (computerScore < 5)) {
-        playRound(getHumanChoice(), getComputerChoice(), round);
-        round = round + 1;
-    }
-    console.clear();
-    if (humanScore === 5) {
-        console.log("You have won the whole thing!")
-    } else {
-        console.log("You have lost the whole thing!")
-    }
-    console.log("      ");
-    console.log("Human score: " + humanScore);
-    console.log("Computer score: " + computerScore);
+function playGame(playerSelection) {
+    playRound(playerSelection, getComputerChoice());
 }
